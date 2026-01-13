84a5ff19a65822b839352fce4d44223995235992
Alex <102387781+savvyaxl@users.noreply.github.com>
2026-01-12 21:06:18 -0300

    Fix formatting issues in run.bat

--- run.bat
+++ run.bat
@@ -9,6 +9,14 @@ rem Get current directory
 set "CurrentDir=%cd%"
 echo Current directory is: %CurrentDir%
 
+set "TargetPath=C:\temp\"
+if exist "%TargetPath%\" (
+    echo Folder exists: %TargetPath%
+) else (
+    echo Folder does not exist: %TargetPath%
+	GOTO :EOF
+)
+
 rem Run PowerShell script and capture output
 for /f "usebackq delims=" %%A in (`powershell -NoProfile -ExecutionPolicy Bypass -File "GetDate.ps1"`) do (
     set "SelectedDate=%%A"
@@ -21,6 +29,5 @@ if "%SelectedDate%"=="UserCancelled" (
 )
 
 echo Selected date is: %SelectedDate%
-
 
 endlocal
